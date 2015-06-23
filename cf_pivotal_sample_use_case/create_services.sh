b3300cc5be9be5eef85e5f1bacc04b9e1dd32e61
WangYi <ywang@pivotal.io>
2015-06-23 17:23:04 +0800

    add services binding for lab 2

---
+++ cf_pivotal_sample_use_case/create_services.sh
@@ -0,0 +1,3 @@
+cf cups address-postal-service -p '{"uri":"http://address-postal.cfapps.io/"}'
+cf cups postal-to-latlon-service -p '{"uri":"http://postal-to-latlon.cfapps.io/"}'
+cf cups region-appender-service -p '{"uri":"http://region-appender.cfapps.io/"}'
